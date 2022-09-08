fcc3bf4d60205fddfb75132010a193ee6ababa5e
Andrew Burkus <da8urk@gmail.com>
2022-09-07 22:02:29 -0400

    clean up useTodo

--- src/hooks/useTodo.ts
+++ src/hooks/useTodo.ts
@@ -1,15 +1,15 @@
 import { useMemo } from "react";
-import { useRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 import store from "store";
 import TodoItem from "types/todo-item";
 import { update, remove, toggleDone } from "lib/todo";
 import { pipe } from "lib/app";
 
 export default function useTodo(item: TodoItem) {
-    const [todos, setTodos] = useRecoilState(store.todoItems)
+    const setTodos = useSetRecoilState(store.todoItems)
 
     return useMemo(() => ({
         remove: () => pipe<TodoItem>(remove, setTodos)(item),
         toggle: () => pipe<TodoItem>(toggleDone, update, setTodos)(item)
-    }), [todos, setTodos])
+    }), [setTodos])
 }
